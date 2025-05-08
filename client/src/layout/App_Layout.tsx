114c38704616a02a1e94552c33045364c430acf6
mariam-elsarag <mariamelsarag44@gmail.com>
2025-05-08 13:53:18 +0200

    layout for page

--- client/src/layout/App_Layout.tsx
+++ client/src/layout/App_Layout.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/ui/sidebar/Sidebar";
 
 const App_Layout = () => {
-  return <div className="bg-pink-50 h-[50vh]">App_Layout</div>;
+  const [openSidebar, setOpenSidebar] = useState(false);
+
+  return (
+    <main className="bg-body p-8 min-h-[100dvh] flex items-start">
+      <Sidebar openSidebar={openSidebar} setOpenSidebar={setOpenSidebar} />
+      <div
+        className={` w-full  md:w-[calc(100%_-_215px)] lg:w-[calc(100%_-_271px)] ms-auto flex flex-col gap-6 `}
+      >
+        <Outlet />
+      </div>
+    </main>
+  );
 };
 
 export default App_Layout;
