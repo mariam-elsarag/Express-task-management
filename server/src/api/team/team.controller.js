a88b637921ff342e18a4da9e626a2e4306b70c5f
mariam-elsarag <mariamelsarag44@gmail.com>
2025-05-08 21:07:03 +0200

    add update invitation route and controller

--- server/src/api/team/team.controller.js
+++ server/src/api/team/team.controller.js
@@ -55,7 +55,7 @@ export const createTeam = asyncWrapper(async (req, res, next) => {
       io.to(socketId).emit("notification", {
         type: "invitation",
         message: `You have been invited to join ${team.name}`,
-        teamId: team._id,
+        type_id: team._id,
         is_invited: true,
       });
     }
@@ -84,6 +84,96 @@ export const createTeam = asyncWrapper(async (req, res, next) => {
   });
 });
 
+// accept or reject invitation
+export const updateInvitationStatus = asyncWrapper(async (req, res, next) => {
+  const userId = req.user._id;
+  const { id: teamId } = req.params;
+  const filterData = serializeBody(req.body, next, ["status"]);
+  if (!filterData) {
+    return;
+  }
+
+  // field validation
+  const rawStatus = filterData.status;
+
+  const status =
+    rawStatus === true || rawStatus === "true"
+      ? true
+      : rawStatus === false || rawStatus === "false"
+      ? false
+      : null;
+  if (status === null) {
+    return next(new AppErrors("Status must be true or false", 400));
+  }
+  // find user with this invitation
+  const user = await Invitation.findOne({
+    team: teamId,
+    "invited_users.user": userId,
+  });
+  if (!user) {
+    return next(new AppErrors("You are not invited to this team", 404));
+  }
+  // move this user to team
+
+  const invitedUser = user.invited_users.at(0);
+
+  if (!invitedUser) {
+    return next(new AppErrors("You are not invited to this team", 403));
+  }
+
+  // step 1 find team
+  if (filterData.status) {
+    const team = await Team.findByIdAndUpdate(
+      teamId,
+      {
+        $push: {
+          members: {
+            user: invitedUser.user,
+            role: invitedUser.role,
+          },
+        },
+      },
+      { new: true }
+    );
+
+    if (!team) {
+      return next(new AppErrors("Team no longer exists", 400));
+    }
+  }
+  logger.info("after add member to team");
+
+  // update notification
+  await Notification.updateMany(
+    {
+      user: userId,
+      type_id: teamId,
+    },
+    {
+      $set: {
+        is_invited: false,
+      },
+    }
+  );
+  // remove user form invitation and if there's no users in list remove this invitation list
+  const updatedInvitation = await Invitation.findOneAndUpdate(
+    { team: teamId },
+    {
+      $pull: {
+        invited_users: { user: userId },
+      },
+    },
+    { new: true }
+  );
+
+  if (updatedInvitation && updatedInvitation.invited_users.length === 0) {
+    await Invitation.deleteOne({ team: teamId });
+  }
+  const message =
+    filterData.status == "true"
+      ? "Successfully accept invitation"
+      : "Successfully reject invitation";
+  res.status(200).json({ message });
+});
 // invite member to team
 export const inviteMemberToTeam = asyncWrapper(async (req, res, next) => {
   const user = req.user._id;
--- server/src/api/team/team.route.js
+++ server/src/api/team/team.route.js
@@ -3,12 +3,23 @@ import multer from "multer";
 
 // middleware
 import { authorized, protect } from "../../middlewares/auth.middleware.js";
-import { createTeam, inviteMemberToTeam } from "./team.controller.js";
+import {
+  createTeam,
+  inviteMemberToTeam,
+  updateInvitationStatus,
+} from "./team.controller.js";
 
 const router = express.Router();
 const upload = multer();
 
-router.use(protect(), authorized("admin", "manager"));
+router.use(protect());
+// routes for usr
+router
+  .route("/:id/invite")
+  .patch(authorized("user"), upload.none(), updateInvitationStatus);
+
+// for route only manager and admin
+router.use(authorized("admin", "manager"));
 router.route("/").post(upload.none(), createTeam);
 router.route("/invite").post(upload.none(), inviteMemberToTeam);
 export default router;
