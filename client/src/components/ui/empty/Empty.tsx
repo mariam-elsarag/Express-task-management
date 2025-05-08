aee6ea2abfe87a5f17f331e484abfb4f912bf9a2
mariam-elsarag <mariamelsarag44@gmail.com>
2025-05-08 18:59:10 +0200

    empty

---
+++ client/src/components/ui/empty/Empty.tsx
@@ -0,0 +1,38 @@
+import React, { ReactNode } from "react";
+import { NotificationWithLine } from "../../../assets/icons/Icon";
+
+interface EmptyType {
+  page: string;
+}
+
+const Empty: React.FC<EmptyType> = ({ page = "" }) => {
+  const handleMessage = () => {
+    switch (page) {
+      case "notification":
+        return {
+          message: "No notification yet",
+          icon: (
+            <NotificationWithLine
+              fill="var(--color-grey-200)"
+              width="60"
+              height="60"
+            />
+          ),
+        };
+
+      default:
+        return "Empty page";
+    }
+  };
+  const { message, icon } = handleMessage();
+  return (
+    <div className="flex_center min-h-[70vh] flex-col gap-3">
+      {icon}
+      <p className="text-grey-200 capitalize font-semibold text-lg">
+        {message}
+      </p>
+    </div>
+  );
+};
+
+export default Empty;
