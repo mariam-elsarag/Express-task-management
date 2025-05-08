06bc67586f961463143f38a7e7cd5b3f216f67d2
mariam-elsarag <mariamelsarag44@gmail.com>
2025-05-08 15:03:39 +0200

    layout for dashboard

--- client/src/layout/App_Layout.tsx
+++ client/src/layout/App_Layout.tsx
@@ -5,14 +5,16 @@ import Sidebar from "../components/ui/sidebar/Sidebar";
 
 const App_Layout = () => {
   const [openSidebar, setOpenSidebar] = useState(false);
-
+  const [hasNotification, setHasNotification] = useState(false);
   return (
-    <main className="bg-body p-8 min-h-[100dvh] flex items-start">
+    <main className="bg-body p-4 min-h-[100dvh] flex items-start">
       <Sidebar openSidebar={openSidebar} setOpenSidebar={setOpenSidebar} />
       <div
-        className={` w-full  md:w-[calc(100%_-_215px)] lg:w-[calc(100%_-_271px)] ms-auto flex flex-col gap-6 `}
+        className={` w-full  md:w-[calc(100%_-_225px)] lg:w-[calc(100%_-_251px)] ms-auto flex flex-col gap-6 `}
       >
-        <Outlet />
+        <Outlet
+          context={{ setOpenSidebar, hasNotification, setHasNotification }}
+        />
       </div>
     </main>
   );
